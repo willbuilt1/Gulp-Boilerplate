2a0254fc2248478b8ab3a7e1edfa5d7ac8ebe272
Will <wfoster12@gmail.com>
2018-08-08 11:30:56 +0100

    Add boilerplate

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,4 @@
+//Where to import modules
+
+import Test from './modules/Test';
+
---
+++ app/assets/scripts/modules/Test.js
@@ -0,0 +1,9 @@
+//Example script
+
+import $ from 'jquery';
+
+$('h1').click(()=>{
+    console.log("success")
+})
+
+export default Test;
